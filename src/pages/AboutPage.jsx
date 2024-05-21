7157d56bf1620592176ffcb92646a063557bf778
Fredtorn <fredrika.tornkvist@chasacademy.se>
2024-05-21 15:03:12 +0200

    fredrikas first commit added about

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,15 @@
+const AboutPage = () => {
+    return (
+        <>
+            <div>
+                <h1>About us</h1>
+                <p>Welcome to Wanderlust Journeys, your ultimate travel companion! Whether you’re a seasoned globetrotter or embarking on your first adventure, we are here to inspire and guide you to your dream destinations.</p>
+            </div>
+            <div>
+                <h2>Who we are</h2>
+            </div>
+        </>
+    )
+}
+
+export default AboutPage;
